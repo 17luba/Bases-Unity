44d346a87f064a6f071c875f18e7f755b08f1c0b
17luba <146106312+17luba@users.noreply.github.com>
2025-03-27 15:24:44 +0100

    Correction exercice 3; quand on clic sur space, notre object change de forme ( sphere, cube,...)

--- Assets/Exercices/Exercice3.cs
+++ Assets/Exercices/Exercice3.cs
@@ -12,7 +12,54 @@ public class Exercice3 : MonoBehaviour
 
     // Code à compléter (ne rien modifier avant cette ligne)
 
-  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TransformObject();
+
+        }
+    }
+    void TransformObject()
+    {
+        PrimitiveType[] types = new PrimitiveType[]
+        {
+        PrimitiveType.Cube,
+        PrimitiveType.Sphere,
+        PrimitiveType.Cylinder,
+        PrimitiveType.Capsule,
+        };
+
+        int randomIndex = Random.Range(0, types.Length);
+        PrimitiveType selectedType = types[randomIndex];
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+
+        if (meshFilter != null)
+        {
+            //GameObject primitive = GameObject.CreatePrimitive(types[randomIndex]);
+            //meshFilter.mesh = primitive.GetComponent<MeshFilter>().mesh;
+
+            string meshName = "";
+            switch (selectedType)
+            {
+                case PrimitiveType.Cube: meshName = "Cube.fbx"; break;
+                case PrimitiveType.Sphere: meshName = "Sphere.fbx"; break;
+                case PrimitiveType.Cylinder: meshName = "Cylinder.fbx"; break;
+                case PrimitiveType.Capsule: meshName = "Capsule.fbx"; break;
+            }
+            Mesh newMesh = Resources.GetBuiltinResource<Mesh>(meshName);
+
+            if (newMesh != null ) 
+            {
+                meshFilter.mesh = newMesh;
+            }
+            else
+            {
+                Debug.LogError("Mesh non trouvé pour " + selectedType);
+            }
+        }
+       
+    }
 
     // Fin du code à compléter (ne rien modifier après cette ligne)
 }
